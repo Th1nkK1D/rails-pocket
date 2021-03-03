0609424381fbd9edd24fcfd7349b873eb910c741
Th1nkK1D <witheep@gmail.com>
2021-03-04 00:29:36 +0700

    Remove unused fetch record in transection reflex

--- app/reflexes/transaction_reflex.rb
+++ app/reflexes/transaction_reflex.rb
@@ -1,20 +1,13 @@
 # frozen_string_literal: true
 
 class TransactionReflex < ApplicationReflex
-  def fetch_transactions
-    return Transaction.all.order(created_at: :desc)
-  end
-
   def add
     transaction = params[:transaction]
+    
     Transaction.create(name: transaction[:name], amount: transaction[:amount].to_f)
-
-    @transactions = fetch_transactions
   end
 
-  def remove
-    Transaction.find_by(id: element.dataset[:id]).destroy
-
-    @transactions = fetch_transactions
+  def remove    
+    Transaction.find_by(id: element.dataset[:id]).destroy!
   end
 end
