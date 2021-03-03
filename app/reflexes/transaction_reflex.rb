6ea2b2c619605a2367e0da37e6c96be8b74c4185
Th1nkK1D <witheep@gmail.com>
2021-03-03 23:25:32 +0700

    Refactor transaction reflex

--- app/reflexes/transaction_reflex.rb
+++ app/reflexes/transaction_reflex.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
 class TransactionReflex < ApplicationReflex
-  def add
-    Transaction.create(name: params[:transaction][:name], amount: params[:transaction][:amount].to_f)
+  def fetch_transactions
+    return Transaction.all.order(created_at: :desc)
+  end
 
-    @transactions = Transaction.all.order(created_at: :desc)
+  def add
+    transaction = params[:transaction]
+    Transaction.create(name: transaction[:name], amount: transaction[:amount].to_f)
+
+    @transactions = fetch_transactions
   end
 
   def remove
     Transaction.find_by(id: element.dataset[:id]).destroy
 
-    @transactions = Transaction.all.order(created_at: :desc)
+    @transactions = fetch_transactions
   end
 end
